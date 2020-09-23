90479d7fce1974252d25b0a8ffb50a5abcd5838d
Geni W <williamsgen@urbandaleschools.com>
2020-09-23 00:25:23 -0500

    triangle

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -5,9 +5,12 @@
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
 username= input("Please enter your name: ")
 print("Hello",username, end= '.')
+print()
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
-
-
+base= input("Please enter the base of the triangle: ")
+height= input("Please enter the height of the triangle: ")
+area= base*height/2
+print("The triangle\'s area is:" ,area,)
 # 3. Write a line of code that will ask the user for the radius of a circle and then prints the circumference.
 
 
