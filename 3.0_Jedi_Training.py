0173986cd1cedf339a2241b9da40066c0534bea0
Geni W <williamsgen@urbandaleschools.com>
2020-09-23 00:13:19 -0500

    name

--- 3.0_Jedi_Training.py
+++ 3.0_Jedi_Training.py
@@ -1,10 +1,10 @@
-# Sign your name:________________
+# Sign your name: Geni W
 # In all the short programs below, do a good job communicating with your end user!
 
 
 # 1.) Write a program that asks someone for their name and then prints a greeting that uses their name.
-
-
+username= input("Please enter your name: ")
+print("Hello",username, end= '.')
 # 2. Write a program where a user enters a base and height and you print the area of a triangle.
 
 
