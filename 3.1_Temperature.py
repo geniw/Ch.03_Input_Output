050a6079fba3081a5640f1023a4ba0ebcc12c46f
Geni W <williamsgen@urbandaleschools.com>
2020-09-29 10:59:27 -0500

    temperature

--- 3.1_Temperature.py
+++ 3.1_Temperature.py
@@ -9,10 +9,9 @@ In: 32  Out: 0
 In: 212  Out: 100
 In: 52  Out: 11.1
 In: 25  Out: -3.9
-In: -40  Out: ??? Please tell me what this output is!
+In: -40  Out: -40 Please tell me what this output is!
 
 '''
-
-
-
-
+fahrenheit= int(input("Please enter the Fahrenheit: "))
+celsius= (fahrenheit-32)/1.8
+print("The Celsius is:" ,celsius,)
